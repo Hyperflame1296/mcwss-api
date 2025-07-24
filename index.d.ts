a451aef16d5bb5ef5105c0a0cc74f0a2e2fdfc9b
Hyperflame1296 <jasircranshaw001@gmail.com>
2025-07-24 09:46:08 -0400

    bro i actually hate TS so much

--- index.d.ts
+++ index.d.ts
@@ -1,46 +1,41 @@
-import { WebSocketServer } from 'ws';
-declare module 'mcwss-api' {
-    export type EventType = 
-        'PlayerMessage' |
-        'PlayerTravelled' |
-        'PlayerTransform' |
-        'PlayerTeleport' |
-        'PlayerDied' |
-        'PlayerBounced' |
-        'EntitySpawned' |
-        'ItemUsed' |
-        'ItemAcquired' |
-        'ItemSmelted' |
-        'ItemCrafted' |
-        'BlockPlaced' |
-        'BlockBroken' |
-        'MobKilled' |
-        'MobInteracted';
-
-    export type MessagePurpose =
-        'commandRequest' |
-        'commandResponse' |
-        'event' |
-        'error'
-    
-    export type WrapperOptions = {
-        logCmdErrors: boolean,
-        logCmdOutput: boolean,
-        logMsgErrors: boolean,
-        logSeriousErrors: boolean,
-        logOther: boolean
-    }
-    export class APIInstance {
-        wss: WebSocketServer | undefined;
-        options: WrapperOptions;
-        start(port: number, host: string, options: WrapperOptions): void;
-        stop(): void;
-        subscribe(ws: WebSocket, event_type: EventType): void;
-        unsubscribe(ws: WebSocket, event_type: EventType): void;
-        on(ws: WebSocket, event_type: EventType, cb: (msg: object) => void): void;
-        on_purpose(ws: WebSocket, purpose: MessagePurpose, cb: (msg: object) => void): void;
-        run_command(ws: WebSocket, command: string): void;
-        send(ws: WebSocket, json: object): void;
-        send_raw(ws: WebSocket, raw: string | ArrayBufferLike): void;
-    }
+declare type EventType = 
+    'PlayerMessage' |
+    'PlayerTravelled' |
+    'PlayerTransform' |
+    'PlayerTeleport' |
+    'PlayerDied' |
+    'PlayerBounced' |
+    'EntitySpawned' |
+    'ItemUsed' |
+    'ItemAcquired' |
+    'ItemSmelted' |
+    'ItemCrafted' |
+    'BlockPlaced' |
+    'BlockBroken' |
+    'MobKilled' |
+    'MobInteracted';
+declare type MessagePurpose =
+    'commandRequest' |
+    'commandResponse' |
+    'event' |
+    'error';
+declare interface WrapperOptions {
+    logCmdErrors: boolean,
+    logCmdOutput: boolean,
+    logMsgErrors: boolean,
+    logSeriousErrors: boolean,
+    logOther: boolean
+}
+declare class APIInstance {
+    wss: WebSocketServer | undefined;
+    options: WrapperOptions;
+    start(port: number, host: string, options: WrapperOptions): void;
+    stop(): void;
+    subscribe(ws: WebSocket, event_type: EventType): void;
+    unsubscribe(ws: WebSocket, event_type: EventType): void;
+    on(ws: WebSocket, event_type: EventType, cb: (msg: object) => void): void;
+    on_purpose(ws: WebSocket, purpose: MessagePurpose, cb: (msg: object) => void): void;
+    run_command(ws: WebSocket, command: string): void;
+    send(ws: WebSocket, json: object): void;
+    send_raw(ws: WebSocket, raw: string | ArrayBufferLike): void;
 }
